d3d35226bf06292d83c024d7cf75c5788e50a579
Timo Böwing <blazko@huepattl.de>
2023-10-21 01:19:17 +0200

    Added some more data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,7 @@
 -- alter sequence myentity_seq restart with 4;
 insert into Person (id, name, active, dateOfBirth) values(1, 'Jane Doe', true, '1977-12-09');
 insert into Person (id, name, active, dateOfBirth) values(2, 'John Doe', true, '1982-12-30');
+insert into Person (id, name, active, dateOfBirth) values(999, 'Flux Capacitor', false, '1955-11-05');
+insert into Person (id, name, active, dateOfBirth) values(5, 'Melissa Smith', true, '1965-02-13');
+insert into Person (id, name, active, dateOfBirth) values(20100, 'Rose Precede', true, '2001-08-22');
+insert into Person (id, name, active, dateOfBirth) values(666, 'Foo Bar "Hello, world!" Baz', true, '2023-10-21');
